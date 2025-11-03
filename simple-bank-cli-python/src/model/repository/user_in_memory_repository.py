be7f6427d8a0c68575fea7a28ddea102cc2a56f9
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-11-03 18:59:07 -0300

    simple-bank-cli-python(feat): implementin new user repository

--- simple-bank-cli-python/src/model/repository/user_in_memory_repository.py
+++ simple-bank-cli-python/src/model/repository/user_in_memory_repository.py
@@ -1,17 +1,25 @@
+from src.model.domain.user import User
+
 class UserInMemoryRepository:
 
     def __init__(self):
         self.users = list()
 
-    def add(self, user):
-        pass
+    def add(self, user) -> None:
+        self.users.append(user)
 
-    def remove(self, email):
-        pass
+    def remove(self, email) -> User:
+        raise(ValueError, 'not implement')
 
-    def get(self):
-        pass
+    def exists_by_email(self, email) -> bool:
+        for user in self.users:
+            if user.email == email:
+                return user
+        return False
 
-    def find_by_email(self, email):
-        pass
+    def find_by_email(self, email) -> User:
+        result_list = list(filter(lambda user: user.email == email, self.users))
+        if len(result_list) == 0:
+            raise ValueError('not find it')
+        return result_list[0] 
 
