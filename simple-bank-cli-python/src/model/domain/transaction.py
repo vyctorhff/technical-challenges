b37ec74d26a4a2a9fdccf1b7e43cfc2e84d109a5
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-11-07 14:40:25 -0300

    simple-bank-cli-python(feat): transaction

--- simple-bank-cli-python/src/model/domain/transaction.py
+++ simple-bank-cli-python/src/model/domain/transaction.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 class Transaction:
 
-    def __init__(self):
-        self.value = None
-        self.user_source = None
-        self.user_target = None
+    def __init__(self, value, user_source, user_target):
+        self.value = value
+        self.user_source = user_source
+        self.user_target = user_target
         self.dt = datetime.today()
+
+    def check_user_source_email(self, email) -> bool:
+        return self.user_source.email == email
