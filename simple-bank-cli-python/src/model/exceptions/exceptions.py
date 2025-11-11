16f9a312e0c56bfa18236222876cbe3bc40b5da3
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-11-11 13:40:59 -0300

    simple-bank-cli-python(feat): add exception for account

--- simple-bank-cli-python/src/model/exceptions/exceptions.py
+++ simple-bank-cli-python/src/model/exceptions/exceptions.py
@@ -14,6 +14,14 @@ class UserInvalidError(Exception):
     def __str__(self):
         return repr(self.value)
 
+class AccountInvalidError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 class SameUserTransactionError(Exception):
 
     def __init__(self, value):
