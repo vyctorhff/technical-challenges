6248ab719ef711c0cb7a09c5ccd28ca09652fa73
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-01-26 20:52:17 -0300

    changes(user_shopping_batch): ftp config; add controller for run batch

---
+++ user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/web/controller/ConsoleImportController.java
@@ -0,0 +1,30 @@
+package br.com.challenge.user_shopping_batch.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/import/dry")
+@RequiredArgsConstructor
+@Slf4j
+public class ConsoleImportController {
+
+    private final Job consoleCsvJob;
+    private final JobLauncher jobLauncher;
+
+    @PostMapping
+    public ResponseEntity<Void> dry() {
+        try {
+            jobLauncher.run(consoleCsvJob, new JobParametersBuilder().toJobParameters());
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("error during console batch ", e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
