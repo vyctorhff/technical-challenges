76070b57a73c2ceb31f6ff94024f7dd05737d68a
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-01-29 18:16:48 -0300

    challenge(user_shopping_batch): creating context for all processing

---
+++ user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/infra/batch/dto/CompleteWorkflowContext.java
@@ -0,0 +1,15 @@
+package br.com.challenge.user_shopping_batch.infra.batch.dto;
+
+public class CompleteWorkflowContext {
+
+    public String getClientFileFtpPath() {
+        return null;
+    }
+
+    public String getClientFileName() {
+        return null;
+    }
+
+    public void setFileDownloadedPath(String filePath) {
+    }
+}
