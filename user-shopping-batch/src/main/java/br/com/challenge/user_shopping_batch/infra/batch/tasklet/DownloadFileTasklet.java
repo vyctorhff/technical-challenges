82979815d8eaf703d6acc53f222b757535c7f846
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-01-29 18:17:40 -0300

    user_shopping_batch(feat): implementing download ftp file tasklet

--- user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/infra/batch/tasklet/DownloadFileTasklet.java
+++ user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/infra/batch/tasklet/DownloadFileTasklet.java
@@ -1,5 +1,8 @@
 package br.com.challenge.user_shopping_batch.infra.batch.tasklet;
 
+import br.com.challenge.user_shopping_batch.infra.batch.dto.CompleteWorkflowContext;
+import br.com.challenge.user_shopping_batch.infra.ftp.service.FtpService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -9,21 +12,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DownloadFileTasklet implements Tasklet {
 
-    public static final String PARAM_FILE_PATH_NAME = "filePath";
+    private final FtpService ftpService;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         log.info("Starting download file from ftp ");
 
+        CompleteWorkflowContext context = getContext(chunkContext);
+        String filePath = ftpService.downloadInTemp(context.getClientFileFtpPath(), context.getClientFileName());
+
+        context.setFileDownloadedPath(filePath);
+
         log.info("Finished download file from ftp with success ");
         return RepeatStatus.FINISHED;
     }
 
-    private String getFilePath(ChunkContext chunkContext) {
-        return chunkContext.getStepContext().getStepExecutionContext()
-                .get(PARAM_FILE_PATH_NAME)
-                .toString();
+    private CompleteWorkflowContext getContext(ChunkContext chunkContext) {
+        return (CompleteWorkflowContext) chunkContext.getStepContext()
+                .getStepExecutionContext()
+                .get("completeWorkflowContext");
     }
 }
