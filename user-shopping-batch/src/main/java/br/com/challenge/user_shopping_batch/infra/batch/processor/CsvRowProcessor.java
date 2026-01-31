0620f12680ea40f66d657dc7c50036d222b6d820
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-01-31 18:39:10 -0300

    user_shopping_batch(refact): moving processor batch

---
+++ user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/infra/batch/processor/CsvRowProcessor.java
@@ -0,0 +1,20 @@
+package br.com.challenge.user_shopping_batch.infra.batch;
+
+import br.com.challenge.user_shopping_batch.infra.batch.dto.CsvShoppingRowInput;
+import br.com.challenge.user_shopping_batch.infra.batch.dto.CsvShoppingRowOutput;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CsvRowProcessor implements ItemProcessor<CsvShoppingRowInput, CsvShoppingRowOutput> {
+
+    @Override
+    public CsvShoppingRowOutput process(CsvShoppingRowInput item) throws Exception {
+        return new CsvShoppingRowOutput(
+                item.name(),
+                item.shoppingDetail(),
+                item.amount(),
+                item.date()
+        );
+    }
+}
