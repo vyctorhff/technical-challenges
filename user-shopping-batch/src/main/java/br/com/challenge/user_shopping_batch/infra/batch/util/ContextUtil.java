7c9f3619bb3924f58ec869a1b7a0c4d7359f238a
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-02-03 17:59:39 -0300

    user_shopping_batch(feat): util for helping set and get job paramns in context

---
+++ user-shopping-batch/src/main/java/br/com/challenge/user_shopping_batch/infra/batch/util/ContextUtil.java
@@ -0,0 +1,22 @@
+package br.com.challenge.user_shopping_batch.infra.batch.util;
+
+import br.com.challenge.user_shopping_batch.infra.batch.enums.JobParamNames;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+public class ContextUtil {
+
+    public static void addJobParamWithString(ChunkContext context,
+                                             JobParamNames jobParamNames,
+                                             String value) {
+        context.getStepContext()
+                .getJobExecutionContext()
+                .put(jobParamNames.getName(), value);
+    }
+
+    public static String getJobParamInContext(ChunkContext chunkContext, JobParamNames paramNames) {
+        return chunkContext.getStepContext()
+                .getStepExecutionContext()
+                .get(paramNames.getName())
+                .toString();
+    }
+}
