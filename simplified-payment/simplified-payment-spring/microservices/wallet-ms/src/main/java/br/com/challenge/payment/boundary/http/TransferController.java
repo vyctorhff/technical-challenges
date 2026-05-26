8ef6e38cd52f0fe676974df39fd2be52975c6d32
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-05-26 14:16:10 -0300

    simplified-payment-spring(wallet-ms): update requests and fix controller

--- simplified-payment/simplified-payment-spring/microservices/wallet-ms/src/main/java/br/com/challenge/payment/boundary/http/TransferController.java
+++ simplified-payment/simplified-payment-spring/microservices/wallet-ms/src/main/java/br/com/challenge/payment/boundary/http/TransferController.java
@@ -3,16 +3,13 @@ package br.com.challenge.payment.boundary.http;
 import br.com.challenge.payment.boundary.http.dto.TransferRequestDTO;
 import br.com.challenge.payment.boundary.http.swagger.TransferControllerSwagger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/transfer")
 public class TransferController implements TransferControllerSwagger {
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Void> transfer(@RequestBody TransferRequestDTO requestDTO) {
         return ResponseEntity.accepted().build();
     }
